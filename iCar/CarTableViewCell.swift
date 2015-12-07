148630a57067a0f435ed60d28964081b3557ca21
Gergely Kőrössy <korossyg@gmail.com>
2015-12-07 23:40:08 +0100

    Car table cell view

---
+++ iCar/CarTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CarTableViewCell.swift
+//  iCar
+//
+//  Created by Gergely Kőrössy on 07/12/15.
+//  Copyright © 2015 Gergely Kőrössy. All rights reserved.
+//
+
+import UIKit
+
+class CarTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
